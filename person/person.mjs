814ce681726b843ebe85eeeedc9f0e13d7a787d8
Thomas Dillard <thomas.dillard.studios@gmail.com>
2018-08-30 14:18:23 -0700

    fixed spacing issue.

--- person/person.mjs
+++ person/person.mjs
@@ -7,18 +7,18 @@ const Person = DefineMap.extend( "Person", {
 
     // `first` is a String, null, or undefined
     // and represents the first name of the person.
-	first: "string",
+    first: "string",
 
     // `last` is a String, null, or undefined
     // and represents the last name of the person.
-	last: "string",
+    last: "string",
 
     // `fullName` is a getter function that
     // returns a string representing
     // the full name of the person.
-	get fullName() {
-		return this.first + " " + this.last;
-	}
+    get fullName() {
+        return this.first + " " + this.last;
+    }
 
 } );
 
