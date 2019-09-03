6556cb111bdd71d75984dca4524915ffb550569e
Manuel Mujica <manuel.expat@gmail.com>
2019-09-03 18:01:25 -0400

    Upgrade to Can 6

--- index.mjs
+++ index.mjs
@@ -2,8 +2,4 @@ import Todo from "./todos/todo.mjs";
 import todoFixture from "./todos/todo-fixture.mjs";
 import Person from "./person/person.mjs";
 
-export {
-    Todo,
-    todoFixture,
-    Person
-};
+export { Todo, todoFixture, Person };
--- person/person.mjs
+++ person/person.mjs
@@ -1,25 +1,22 @@
-import {DefineMap} from "//unpkg.com/can@5/core.mjs";
+import { ObservableObject } from "//unpkg.com/can@pre/core.mjs";
 
 // -------------------------------
 // Define an observable Todo type:
 // -------------------------------
-const Person = DefineMap.extend( "Person", {
+class Person extends ObservableObject {
+	static props = {
+		// `first` is a String and represents the first name of the person.
+		first: String,
 
-    // `first` is a String, null, or undefined
-    // and represents the first name of the person.
-    first: "string",
+		// `last` is a String and represents the last name of the person.
+		last: String,
 
-    // `last` is a String, null, or undefined
-    // and represents the last name of the person.
-    last: "string",
-
-    // `fullName` is a getter function that
-    // returns a string representing
-    // the full name of the person.
-    get fullName() {
-        return this.first + " " + this.last;
-    }
-
-} );
+		// `fullName` is a getter function that returns a string
+		// representing the full name of the person.
+		get fullName() {
+			return this.first + " " + this.last;
+		}
+	};
+}
 
 export default Person;
